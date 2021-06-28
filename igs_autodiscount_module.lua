a793468629738eb0bd5fb20ff4850d616013407f
Dobytchick <48864677+Dobytchick@users.noreply.github.com>
2021-06-29 05:28:48 +0600

    Add blacklist category

--- igs_autodiscount_module.lua
+++ igs_autodiscount_module.lua
@@ -16,19 +16,25 @@ http.Fetch("https://date.nager.at/api/v2/PublicHolidays/" .. os.date("%Y", os.ti
     HolidaysTable = util.JSONToTable(code)
 end)
 
+local DiscountBlacklist = {}
 HolidaysTable = HolidaysTable or {}
+
+local function AddBlackCategory(sCat)
+	DiscountBlacklist[sCat] = true
+end
 
 local function AddCustomHoliday(Sname, Sdate)
     if HolidaysTable[#HolidaysTable - 1].localName == Sname then return false end
     HolidaysTable[#HolidaysTable + 1] = {localName = Sname, date = Sdate}
 end
 
-
 local WeekendDiscountEnabled = true     -- Будут ли действовать скидки по выходным
 local WeekendDiscount = 20            -- Сколько будет действовать процентов скидка на товары
 
 local HolidayDiscount = 50              -- Сколько будет действовать процентов скидка на товары
 local HolidayDuration = 7               -- Сколько будут действовать скидки после начала праздника (в днях)
+
+AddBlackCategory("КатегорияНейм") -- Добавление категории, на которую не будут действовать скидки
 
 -- Убираем нахуй никому ненужные данные в таблице ¯\_(ツ)_/¯
 for k,v in pairs(HolidaysTable) do
@@ -53,11 +59,13 @@ weekTable["Sunday"] = 1
 
 if weekTable[os.date("%A", os.time())] and WeekendDiscountEnabled then
     for k,v in ipairs(IGS.GetItems()) do
-        local old_price = v:Price()
-        local new_price = old_price * 0.8
+	if !DiscountBlacklist[v.category] then
+		local old_price = v:Price()
+		local new_price = old_price * 0.8
 
-        v:SetPrice(new_price)
-        v:SetDiscountedFrom(old_price)
+		v:SetPrice(new_price)
+		v:SetDiscountedFrom(old_price)
+	end
     end
 
     if SERVER then
